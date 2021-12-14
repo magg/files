bd50e680e2791e762c438c52ca6967be7cd68352
Miguel Gonzalez <miguel@okaythis.com>
2021-12-13 22:21:14 -0600

    added multi project pom

--- src/main/java/com/example/file/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.file.repository;
-
-import com.example.file.domain.AccountDomain;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AccountRepository extends MongoRepository<AccountDomain, String>
-{
-}
--- src/main/java/com/example/file/storage/FilePointer.java
+++
@@ -1,9 +0,0 @@
-package com.example.file.storage;
-
-import java.io.InputStream;
-
-public interface FilePointer
-{
-
-    InputStream open();
-}
